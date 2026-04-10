8b2d84f3bb86d62dd16ce22f84a1fb999857cf29
bhsnyounes13 <bhsnyounes13@gmail.com>
2026-04-10 13:20:38 -0700

    Fresh build

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 header('Content-Type: application/json');
-echo json_encode(['status' => 'ok', 'message' => 'API running']);
+echo json_encode(['status' => 'ok', 'version' => '2', 'message' => 'API running']);
